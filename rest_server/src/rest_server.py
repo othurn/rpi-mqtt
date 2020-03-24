bb839e8202e618b0f7b3003e3e22485bf626ecab
Oliver <othurn@ucsd.edu>
2020-03-24 14:50:20 -0700

    base to rest completed

--- rest_server/src/rest_server.py
+++ rest_server/src/rest_server.py
@@ -2,18 +2,79 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 import json
 
+BED_ROOM = 'data/bedroom.json'
+LIVING_ROOM = 'data/livingroom.json'
 
-def get_books(req):
+def open_json(file_path):
+  with open(file_path, 'r') as db_file:
+    json_obj = json.load(db_file)  
+    db_file.close()
+  return json_obj
+
+def get_test(req):
   with open('data/test.txt', 'r') as db_file:
     books = json.load(db_file)
   return books
 
+def get_bedroom(req):
+  json_data = open_json(BED_ROOM)
+  return json_data
+
+
+def get_living_room(req):
+  json_data = open_json(LIVING_ROOM)
+  return json_data
+
+def post_bedroom(req):
+  # confirm post req
+  print('bedroom post accepted')
+  text = req.text       # gets just the data that was sent
+  print(text)
+    
+  bedroom = open_json(BED_ROOM)
+  print(bedroom)
+  
+  bedroom.append(text)
+  print(bedroom)
+  
+  bedroom_json = json.dumps(bedroom)
+  print(bedroom_json)
+  
+  f = open('data/bedroom.json', 'w') 
+  f.write(bedroom_json)
+  f.close()
+  
+  # need to return something
+  return 'ok'
+    
+def post_livingroom(req):
+  
+  print('living room post accepted')
+  payload = req.text
+  living_room = open_json(LIVING_ROOM)
+  living_room.append(payload)
+  
+  with open('data/livingroom.json', 'w') as f:
+    f.write(json.dumps(living_room))
+    f.close()
+  
+  return 'ok'
 
 if __name__ == '__main__':
   config = Configurator()
 
-  config.add_route('rest_route', '/test')
-  config.add_view(get_books, route_name='rest_route', renderer='json')
+  config.add_route('test', '/test')
+  config.add_view(get_test, route_name='test', renderer='json')
+  
+  config.add_route('bedroom', '/bedroom')
+  config.add_view(get_bedroom, route_name='bedroom', renderer='json')
+  config.add_view(post_bedroom, route_name='bedroom', request_method="POST", renderer='json')
+  
+  config.add_route('livingroom', '/livingroom')
+  config.add_view(get_living_room, route_name='livingroom', renderer='json')
+  config.add_view(post_livingroom, route_name='livingroom', request_method="POST",
+                  renderer='json')
+
 
   app = config.make_wsgi_app()
   server = make_server('0.0.0.0', 7070, app)
--- rpi/base.py
+++ rpi/base.py
@@ -3,15 +3,23 @@
 import paho.mqtt.client as mqtt
 import re
 import json
+import requests
 
 # Server (broker) settings
 
-broker = "192.168.0.19"  # rpi
-# broker = "localhost"
+# broker = "192.168.0.19"  # rpi
+broker = "localhost"
 port = 1883  # port
 
 sub_topics = ['/base/test']
 pub_topics = ['/Zero_1/test','/3A/test']
+
+def post_to_rest():
+    data = {'title': 'fake title', 'author': 'fake author'}
+    data = json.dumps(data)
+    requests.post('http://localhost:7071/bedroom/temp', data=data)
+    requests.post('http://localhost:7071/livingroom', data=data)
+    
 
 
 def cust_on_connect(client, userdata, flags, rc):
@@ -35,8 +43,10 @@ client.on_connect = cust_on_connect
 client.on_message = cust_on_message
 client.on_publish = cust_on_publish
 
+post_to_rest()
+
 message = 'Hey from Base'
 for pub in pub_topics:
     client.publish(pub, message, 0)
 
-client.loop_forever()
+client.loop()
