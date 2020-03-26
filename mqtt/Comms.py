b3f6119fee4101321c0d91b42a319bf8f419966b
Oliver <othurn@ucsd.edu>
2020-03-26 01:06:02 -0700

    refactored base class and bedroom

--- mqtt/Comms.py
+++ mqtt/Comms.py
@@ -6,8 +6,10 @@ PORT = 1883
 
 class Communication:
     
-    def __init__(self, client_name):
+    def __init__(self, client_name, sub_topics:list, pub_topics:list):
         self.client_name = client_name
+        self.sub_topics = sub_topics
+        self.pub_topics = pub_topics
         
         self.publisher = mqtt.Client(f'{client_name}_publisher')
         self.subscriber = mqtt.Client(f'{client_name}_subscriber')
@@ -18,10 +20,18 @@ class Communication:
         
         
     def subscribe_all(self): 
-        self.subscriber.subscribe('/class_topic')    
+        self.subscriber.subscribe('/class_topic')
+        
+        for sub in self.sub_topics:
+            self.subscriber.subscribe(sub, 0)   
+            print(self.client_name + " subscribed to " + sub) 
     
     def publish_all(self):
         self.publish_test()
+        
+        
+    def publish_to_topic(self, topic, payload):
+        self.publisher.publish(topic, payload, 0)
         
         
     def publish_test(self):
